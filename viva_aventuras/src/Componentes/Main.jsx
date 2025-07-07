9a122c3537eca7568d5112abd6309ac682c4002c
lcostaju <luciocj_eng@outlook.com>
2025-07-06 21:25:18 -0300

    feat: add ComoFuncionaSection component with styles and integrate into Main component

--- viva_aventuras/src/Componentes/Main.jsx
+++ viva_aventuras/src/Componentes/Main.jsx
@@ -3,6 +3,7 @@ import MontainSection from "./MontainSection/MontainSection";
 import NossasExperiencias from "./NossasEsperiencias/NossasExperiencias";
 
 import FormSection from "./Formulario/FormSection";
+import ComoFuncionaSection from "./ComoFunciona/ComoFuncionaSection";
 
 
 function Main(){
@@ -10,6 +11,7 @@ function Main(){
     return(
         <main>
         <NossasExperiencias/>
+        <ComoFuncionaSection/>
         <FormSection />
 
         </main>

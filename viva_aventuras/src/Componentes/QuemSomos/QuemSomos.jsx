eccf7c04b9cd3e0747fa3f15b48a92b154c88ae6
lcostaju <luciocj_eng@outlook.com>
2025-07-09 20:13:15 -0300

    fix: correct text description in NossasExperiencias and QuemSomos components

--- viva_aventuras/src/Componentes/QuemSomos/QuemSomos.jsx
+++ viva_aventuras/src/Componentes/QuemSomos/QuemSomos.jsx
@@ -14,7 +14,7 @@ function QuemSomos() {
         <p className={styles['quem-somos-desc']}>
             Viva Aventuras é uma operadora de ecoturismo especializada em turismo de aventura
             com base na segurança, sustentabilidade e inclusão das mulheres no Turismo de Aventuras
-            e ao esporte atraves dos curso de Rapel. Fundada em 2019 ,opera na região do Triângulo Mineiro e Canastra. Nestes 6 anos realizou vários curso de Rapel esportivo e dese de 2023 oferece tambem Cursos profissionais como Trabalho em Altura conforme Nr35. Tornando 
+            e ao esporte atraves dos curso de Rapel. Fundada em 2019 ,opera na região do Triângulo Mineiro e Canastra. Nestes 6 anos realizou vários curso de Rapel esportivo e desde de 2023 oferece tambem Cursos profissionais como Trabalho em Altura conforme Nr35. Tornando 
             profissionais com alto padrão técnico e também realizamos Consultoria e treinamentos profissionais
             pecializados para empresas no setor de Limpeza e Conservação.
             Nosso impacto vai além da adrenalina: preservamos o meio ambiente, promovemos o
